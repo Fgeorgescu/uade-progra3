9279c25af075c2228641c0c22e2dbe3893e80e4d
fgeorgescu <franco.georgescu@mercadolibre.com>
2021-02-21 00:45:52 -0300

    polish Dijkstra and add loggs

--- src/main/java/TPO/dijsktra/Dijsktra.java
+++ src/main/java/TPO/dijsktra/Dijsktra.java
@@ -17,8 +17,7 @@ import java.util.logging.Logger;
 
 public class Dijsktra extends JApplet{
 
-    private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
-
+    private static final Double INFINITO = -1.0;
 
     private static final Logger logger = LoggerFactory.getLogger(Dijsktra.class.getSimpleName());
 
@@ -41,7 +40,7 @@ public class Dijsktra extends JApplet{
         verticesSet.forEach((vertice -> {
             logger.finer("Agregamos " + vertice + " a la cola, y ponemos los valores -1 y \"\" en distancia y previo");
             q.add(vertice);
-            dist.put(vertice, -1.0);
+            dist.put(vertice, INFINITO);
             prev.put(vertice, "");
         }));
 
@@ -56,18 +55,57 @@ public class Dijsktra extends JApplet{
             Set<DefaultWeightedEdge> aristasSalientes = stringGraph.outgoingEdgesOf(verticeActual);
             logger.fine("Las aristas salientes son: " + aristasSalientes.toString());
 
-            aristasSalientes.forEach(saliente -> {
-                logger.fine("Analizamos la arista " + saliente.toString());
-                String destino = stringGraph.getEdgeTarget(saliente);
-                double pesoArista = stringGraph.getEdgeWeight(saliente);
-                logger.fine(String.format("La distancia de %s a %s es de %f", verticeActual, destino, pesoArista));
+            // Analizando todas las aristas salientes, determino los pesos para llegar a los destinos de las mismas
+            aristasSalientes.forEach(aristaSaliente -> {
+                logger.fine("Analizamos la arista " + aristaSaliente.toString());
 
-                double pesoCamino = dist.get(verticeActual) + pesoArista;
+                //Calculo el destino y peso de la arista
+                String destino = stringGraph.getEdgeTarget(aristaSaliente);
+                double pesoArista = stringGraph.getEdgeWeight(aristaSaliente);
+                logger.fine(String.format("El peso de la arista (%s:%s) es de %f, y el peso hasta %s es %f",
+                        verticeActual,
+                        destino,
+                        pesoArista,
+                        verticeActual,
+                        dist.get(verticeActual))
+                );
 
-                if (pesoCamino < dist.get(verticeActual)) {
-                    dist.put(verticeActual, pesoArista);
-                    prev.put(stringGraph.getEdgeTarget(saliente), verticeActual);
-                }
+                double pesoCaminoHastaDestino = dist.get(verticeActual) + pesoArista;
+                 logger.fine(String.format("El peso acumulado desde el origen pasando por (%s:%s) sería %f",
+                         verticeActual,
+                         destino,
+                         pesoCaminoHastaDestino)
+                 );
+
+                // Si el peso hasta el destino, reemplazo el peso y el antecesor por los valores correspondientes
+                // Peso == -1
+                 if (dist.get(destino).equals(INFINITO) || pesoCaminoHastaDestino < dist.get(destino)) {
+                    logger.fine(String.format("El nuevo peso para %s es mejor que el existente (%f < %s)",
+                            destino,
+                            pesoCaminoHastaDestino,
+                            dist.get(destino).equals(INFINITO) ? "Infinito" : dist.get(destino))
+                    );
+
+                    logger.fine(String.format("Actualizamos el peso hasta el vertice %s por %f",
+                            destino,
+                            pesoCaminoHastaDestino)
+                    );
+
+                    dist.put(destino, pesoCaminoHastaDestino);
+                    logger.fine(String.format("Actualizamos el previo del destino %s por el vertice actual %s",
+                            destino,
+                            verticeActual)
+                    );
+
+                    prev.put(stringGraph.getEdgeTarget(aristaSaliente), verticeActual);
+                } else {
+                     logger.fine(String.format("El nuevo peso para %s es peor que el existente (%f > %s)",
+                             destino,
+                             pesoCaminoHastaDestino,
+                             dist.get(destino).equals(INFINITO) ? "Infinito" : dist.get(destino))
+                     );
+
+                 }
             }); // https://stackoverflow.com/questions/30876939/getting-all-edges-going-out-from-a-node-in-jgrapht
         }
 
